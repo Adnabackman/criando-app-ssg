c74680144503cfbc48cc47a29af159fa889ed401
Adnabackman <adnabackman@gmail.com>
2025-03-20 11:30:57 -0300

    criando explore e home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../types/post';
+import { FeedComponent } from '../../components/feed/feed.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [FeedComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  posts: Post[] = [];
+  constructor(private httpClient: HttpClient) {}
 
-}
+  ngOnInit(): void {
+    this.fetchData();
+      
+  }
+    fetchData(){
+      this.httpClient.get<{ posts: Post[]}> ('https://localhost:3000/friends')
+       .subscribe((data: { posts: Post[]}) => {
+        this.posts = data.posts; 
+      });
+    }
+  }
