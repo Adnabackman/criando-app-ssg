3a6eeadbfd604c09269ca9c8beb891ba7f94d319
Adnabackman <adnabackman@gmail.com>
2025-03-20 10:53:46 -0300

    config: prerender

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,14 +1,33 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { ServerRoute, RenderMode } from '@angular/ssr';
+import { provideRouter } from '@angular/router';
 
-const serverConfig: ApplicationConfig = {
+const routes: ServerRoute[] = [
+  {
+    path: 'user/:userId',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: (): Promise<Record<string, string>[]> => {
+      return new Promise((resolve) => {
+        const fs = require('fs');
+        const routesFileContent = fs.readFileSync('route.txt', 'utf-8');
+        const routesArray = routesFileContent.split('\n');
+        const params = routesArray
+          .filter((route: string) => route.startsWith('/user/'))
+          .map((route: string) => {
+            const userId = route.split('/')[2];
+            return { userId: userId };
+          });
+        resolve(params);
+      });
+    },
+  },
+];
+
+export const config: ApplicationConfig = mergeApplicationConfig(appConfig, {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
-  ]
-};
-
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+    provideRouter(routes)
+  ],
+});
