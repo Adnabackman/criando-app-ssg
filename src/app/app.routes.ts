85a8423f21718054c03e815f2c44ab2441098d7b
Adnabackman <adnabackman@gmail.com>
2025-03-21 11:39:29 -0300

    config final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,19 @@
 import { Routes } from '@angular/router';
 import { PostComponent } from './components/post/post.component';
 import { ExplorePageComponent } from './pages/explore-page/explore-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
     {
-        component: PostComponent, 
-        path: "/user/:userId"
+        component: HomePageComponent, 
+        path: ""
     },
     {
         component: ExplorePageComponent, 
-        path: "/explore/:country"
+        path: "explore/:country"
     },
     {
-        path: "", 
+        path: "**", 
         redirectTo: '', 
     }
 ];
